996cb76ad80b8af2c63535a7ae218659392f514f
Carson Lam <carsonyylam@gmail.com>
2018-02-17 21:13:29 -0800

    Add error tests for GTFSRT and RTDS.

--- tests/test_gtfsrt.py
+++ tests/test_gtfsrt.py
@@ -1,6 +1,10 @@
-import pytest
-from tilapia.gtfsrt import GTFSRT
 from datetime import datetime
+
+import pytest
+from requests import codes
+
+from tilapia.errors import TransLinkAPIError
+from tilapia.gtfsrt import GTFSRT
 
 
 @pytest.fixture
@@ -30,3 +34,11 @@ def test_headers_position(authed_gtfs):
 def test_download_position(authed_gtfs, tmpdir):
     dest = tmpdir.join('gtfsposition.pb')
     assert authed_gtfs.download_position(dest)
+
+
+def test_gtfsrt_invalid_key():
+    with pytest.raises(TransLinkAPIError) as info:
+        GTFSRT(api_key='foobar').headers_realtime()
+    assert info.value.response.status_code == codes.forbidden
+    assert not info.value.code
+    assert not info.value.message
--- tests/test_rtds.py
+++ tests/test_rtds.py
@@ -1,7 +1,11 @@
-import pytest
-from tilapia.rtds import RTDS
-from datetime import datetime
 import imghdr
+from datetime import datetime
+
+import pytest
+
+from tilapia.errors import TransLinkAPIError
+from tilapia.rtds import RTDS
+from requests import codes
 
 
 @pytest.fixture
@@ -30,7 +34,31 @@ def test_live_data_at_point_timestamp(authed_rtds):
         -123.04550170898438, 49.23194729854554, z=12, types=6)
     assert isinstance(result.timestampUtc, datetime)
 
+    with pytest.raises(TransLinkAPIError) as info:
+        authed_rtds.live_data_at_point('abc', -1, z=0, types=-1)
+    assert info.value.response.status_code == codes.bad
+    assert info.value.message
+
 
 def test_colour_legend(authed_rtds):
     result = authed_rtds.colour_legend()
     assert isinstance(result, list)
+
+
+def test_tile_bad_key(tmpdir):
+    # Expecting a JSON error response when PNG request is rejected for bad key.
+    dest = tmpdir.join('test.png')
+    with pytest.raises(TransLinkAPIError) as info:
+        RTDS(api_key='foobar').tile(dest, 647, 1402, 12)
+    assert info.value.response.status_code == codes.bad
+    assert not info.value.code
+
+
+@pytest.mark.parametrize('key', ['foobar', 'x' * 20])
+def test_bad_key(key):
+    # There's a different error message for keys that aren't 20 characters.
+    with pytest.raises(TransLinkAPIError) as info:
+        RTDS(api_key=key).all_live_data()
+    assert info.value.response.status_code == codes.bad  # It's never 403.
+    assert not info.value.code
+    assert info.value.message
