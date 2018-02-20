57a22a56716c24d8044eb2110cc5317b3aacf799
Carson Lam <carsonyylam@gmail.com>
2018-02-19 20:04:37 -0800

    Bring public API into init file.

--- tilapia/__init__.py
+++ tilapia/__init__.py
@@ -1,1 +1,5 @@
 from .__version__ import __title__, __author__, __email__, __version__
+from .rtds import RTDS
+from .rtti import RTTI
+from .gtfsrt import GTFSRT
+from .errors import TransLinkAPIError
