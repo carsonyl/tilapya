3a84b7467a11e868961803cd290f461a1a496492
Carson Lam <carsonyylam@gmail.com>
2018-08-04 23:36:28 -0700

    Keep up with Marshmallow prereleases.

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open('HISTORY.rst') as history_file:
 
 requirements = [
     'requests>=2.18.4,<3.0.0',
-    'marshmallow>=3.0.0b7,<4.0.0',
+    'marshmallow>=3.0.0b13,<4.0.0',
     'pytz',
 ]
 
